9a7c0e216f1c22ce9736a16c7d766ce63d6ae889
Jelle Vink <jelle.vink@gmail.com>
2019-09-12 03:07:22 -0700

    Formalize package (#1)
    * Travis build
    * Docker setup
    * License information

--- src/Application.php
+++ src/Application.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace Sugarcrm\PasswordHash;
+/*
+ * This file is part of the skymeyer/sugarcrm-pwd-hash package.
+ *
+ * (c) Jelle Vink <jelle.vink@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Skymeyer\Sugarcrm\PasswordHash;
 
 use Symfony\Component\Console\Application as Base;
 
